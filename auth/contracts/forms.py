45172d8984c85b83c47dbf163cb8910a4227651b
AlexFIDA <aleksandrpapasvili505@gmail.com>
2024-12-04 12:36:58 +0500

    Цель и личный кабинет
    Ссылка в личном кабинете теперь отправляет на договоры, а также можно указать цель при создании контракта

--- auth/contracts/forms.py
+++ auth/contracts/forms.py
@@ -4,7 +4,14 @@ from .models import Contract
 class ContractForm(forms.ModelForm):
     class Meta:
         model = Contract
-        fields = ['amount', 'charity_name', 'charity_account', 'due_date']
+        fields = ['amount', 'charity_name', 'charity_account', 'due_date', 'goal']
+        labels = {
+            'amount': 'Сумма',
+            'charity_name': 'Название фонда',
+            'charity_account': 'Реквизиты фонда',
+            'due_date': 'Дата завершения',
+            'goal': 'Цель',
+        }
         widgets = {
             'due_date': forms.DateInput(attrs={'type': 'date'}),
         }
--- auth/contracts/models.py
+++ auth/contracts/models.py
@@ -9,12 +9,14 @@ class Contract(models.Model):
     ]
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='contracts')
-    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Сумма договора
-    charity_name = models.CharField(max_length=255)               # Название фонда
-    charity_account = models.CharField(max_length=255)            # Реквизиты фонда
-    due_date = models.DateField()                                 # Дата завершения
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    amount = models.DecimalField(max_digits=10, decimal_places=2,  verbose_name="Сумма")  # Сумма договора
+    charity_name = models.CharField(max_length=255, verbose_name="Название Фонда")               # Название фонда
+    charity_account = models.CharField(max_length=255,  verbose_name="Реквизиты Фонда")            # Реквизиты фонда
+    due_date = models.DateField( verbose_name="Дата завершения")                                 # Дата завершения
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending',  verbose_name="Статус")
     created_at = models.DateTimeField(auto_now_add=True)          # Дата создания договора
+    goal = models.TextField(null=True, blank=True,  verbose_name="Цель")  # Добавлено поле для цели
+
 
     def __str__(self):
         return f"Contract {self.id} by {self.user.username}"
